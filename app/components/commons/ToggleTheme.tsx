98840a78d9b45b941ef742b9aa203a7c7250defa
Mariana Almeida <marianaalmeidafga@gmail.com>
2025-01-28 09:48:17 -0300

    feat: css adicionado no toggle

--- app/components/commons/ToggleTheme.tsx
+++ app/components/commons/ToggleTheme.tsx
@@ -9,7 +9,7 @@ import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
 export default function ToggleTheme() {
     return (
         <ul className={style.toggletheme}>
-            <li><FontAwesomeIcon icon={faSun} /></li>
+            <li className={style.toggletheme__select}><FontAwesomeIcon icon={faSun} /></li>
             <li><FontAwesomeIcon icon={faMoon} /></li>
         </ul>
     )
