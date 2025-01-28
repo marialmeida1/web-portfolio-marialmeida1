20264281104617d5ed3da9b67fbd61980b7ea6b0
Mariana Almeida <marianaalmeidafga@gmail.com>
2025-01-28 10:00:34 -0300

    feat: toggle de theme finalizado

--- app/components/commons/ToggleTheme.tsx
+++ app/components/commons/ToggleTheme.tsx
@@ -1,16 +1,44 @@
+'use client';
 // Style
-import style from './ToggleTheme.module.css'
+import style from './ToggleTheme.module.css';
 
 // Icons
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
 
+// React
+import { useState, useEffect } from 'react';
 
 export default function ToggleTheme() {
+    const [isDarkMode, setIsDarkMode] = useState(false);
+
+    // Atualiza o tema no DOM quando o estado muda
+    useEffect(() => {
+        document.documentElement.setAttribute(
+            'data-theme',
+            isDarkMode ? 'dark' : 'light'
+        );
+    }, [isDarkMode]);
+
+    // Alterna o estado de tema
+    function toggleTheme() {
+        setIsDarkMode((prevMode) => !prevMode);
+    }
+
     return (
         <ul className={style.toggletheme}>
-            <li className={style.toggletheme__select}><FontAwesomeIcon icon={faSun} /></li>
-            <li><FontAwesomeIcon icon={faMoon} /></li>
+            <li
+                onClick={toggleTheme}
+                className={!isDarkMode ? style.toggletheme__select : ''}
+            >
+                <FontAwesomeIcon icon={faSun} />
+            </li>
+            <li
+                onClick={toggleTheme}
+                className={isDarkMode ? style.toggletheme__select : ''}
+            >
+                <FontAwesomeIcon icon={faMoon} />
+            </li>
         </ul>
-    )
+    );
 }
