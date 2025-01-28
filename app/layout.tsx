c8da64f4f95a5759d1826f6b1979ce5615001554
Mariana Almeida <marianaalmeidafga@gmail.com>
2025-01-27 22:06:52 -0300

    feat: adicionando a fonte poppins

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,10 @@
-import type { Metadata } from "next";
 import "./globals.css";
+import { Poppins } from 'next/font/google';
+
+const poppins = Poppins({
+  subsets: ['latin'], // Suporte para caracteres em português
+  weight: ['400', '500', '600', '700', '900'], // Selecione os pesos desejados
+});
 
 export default function RootLayout({
   children,
@@ -8,7 +13,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
+      <body className={poppins.className}>
         {children}
       </body>
     </html>
