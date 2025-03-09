fa6842fe125b68e8d1b09f533b834cd9c436a6f7
Mariana Almeida <marianaalmeidafga@gmail.com>
2025-03-08 22:53:25 -0300

    feat: banner web finalizado

--- app/components/home/Banner.tsx
+++ app/components/home/Banner.tsx
@@ -12,7 +12,14 @@ export default function Banner() {
                 <div className={style.banner__overlay_text}>
                     <p>Prazer, sou a Mariana,</p>
                     <h1>Desenvolvedora Full-Stack e Designer UX/UI</h1>
+                    <img src="/img/banner/circles.png" alt="Picture Me" className={style.banner__circles} />
                 </div>
+
+                <img src="/img/banner/picture_elements.png" alt="Picture Me" className={style.banner__picture} />
+
+                {/* Linhas */}
+                <img src="/img/banner/line_01.png" alt="Picture Me" className={style.banner__line01} />
+                <img src="/img/banner/line_02.png" alt="Picture Me" className={style.banner__line02} />
             </div>
         </section>
     );
