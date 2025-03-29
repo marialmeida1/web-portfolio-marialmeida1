5b595e2b9836774b0004e6c1de7f349197e0221e
Mariana Almeida <marianaalmeidafga@gmail.com>
2025-03-29 10:56:52 -0300

    feat: create sections projects

---
+++ app/components/home/Projects.tsx
@@ -0,0 +1,10 @@
+// Styles
+import style from "./Projects.module.css"
+
+export default function Projects() {
+    return (
+        <section className={`container ${style.projects}`}>
+            <h1>Projetos</h1>
+        </section>
+    );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Navbar from "./components/commons/Navbar";
 import About from "./components/home/About";
 import Banner from "./components/home/Banner";
 import IconsLine from "./components/home/IconsLine";
+import Projects from "./components/home/Projects";
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
       <Banner />
       <About />
       <IconsLine />
+      <Projects />
     </div>
   );
 }
