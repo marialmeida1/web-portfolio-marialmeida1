bc3d9662a314f38570ed0ea0cb2fe046b373c5aa
Mariana Almeida <marianaalmeidafga@gmail.com>
2025-04-12 09:38:12 -0300

    feat: contact finish

--- app/components/home/Contact.tsx
+++ app/components/home/Contact.tsx
@@ -1,13 +1,20 @@
+// Styles
 import style from './Contact.module.css'
+
+// Icons
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faWhatsapp } from '@fortawesome/free-brands-svg-icons';
 
 export default function Contact() {
     return (
         <section className={`container ${style.contact}`}>
-            <h1>Contato</h1>
-            <p>Se minhas habilidades técnicas e sociais chamaram sua atenção e você está pronto para criar soluções inovadoras,
-                entre em <br/> contato comigo ou conecte-se pelas minhas redes sociais abaixo.
-                <b>Vamos transformar ideias em realidade!</b></p>
-                <a>Entrar em Contato</a>
+            <div>
+                <h1>Contato</h1>
+                <p>Se minhas habilidades técnicas e sociais chamaram sua atenção e você está pronto para criar soluções inovadoras,
+                    entre em <br /> contato comigo ou conecte-se pelas minhas redes sociais abaixo. <br />
+                    <b>Vamos transformar ideias em realidade!</b></p>
+            </div>
+            <a className={style.contact__btn}>Entrar em Contato <FontAwesomeIcon icon={faWhatsapp} /></a>
         </section>
     );
 }
