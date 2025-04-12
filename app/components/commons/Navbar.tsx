80482bc801fef0ad5b527bc7df65f9afe78ba56b
Mariana Almeida <marianaalmeidafga@gmail.com>
2025-04-12 17:14:15 -0300

    feat: changing function of navbar

--- app/components/commons/Navbar.tsx
+++ app/components/commons/Navbar.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import { useEffect, useState } from "react";
 // Style
 import style from "./Navbar.module.css";
 
@@ -12,17 +13,33 @@ import { usePathname } from "next/navigation";
 
 export default function Navbar() {
 
+    const [notAtTop, setNotAtTop] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            setNotAtTop(window.scrollY > 400);
+        };
+
+        window.addEventListener('scroll', handleScroll);
+        handleScroll();
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
     const pathname = usePathname();
     const isHomePage = pathname === "/";
 
     return (
-        <header className={style.navbar}>
-            <h1 className={`${style.navbar__title} ${isHomePage ? style.white__text : ""}`}>&lt;Mariana/&gt;</h1>
+        <header className={`${style.navbar} ${isHomePage ? "" : style.bg__color} ${notAtTop ? style.bg__color : ''}`}>
+            <h1 className={`${style.navbar__title} ${isHomePage && !notAtTop ? style.white__text : ""}`}>&lt;Mariana/&gt;</h1>
 
             <ul className={style.navbar__actions}>
                 <li><ToggleLanguage /></li>
                 <li><ToggleTheme /></li>
             </ul>
         </header>
+
     )
 }
