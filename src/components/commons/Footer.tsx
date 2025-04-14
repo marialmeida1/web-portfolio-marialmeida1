509fcb350c66dc4e9a90d03c8012a14b82ae4df2
Mariana Almeida <marianaalmeidafga@gmail.com>
2025-04-14 08:16:51 -0300

    feat: create footer component

---
+++ src/components/commons/Footer.tsx
@@ -0,0 +1,5 @@
+import style from "./Footer.module.css"
+
+export default function Footer() {
+    <section className={style.footer}>Copyright © 2025 Mariana Almeida. Todos os direitos reservados.</section>
+}
