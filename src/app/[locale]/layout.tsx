c270e4328f82e2cdf430b27dd716932756bf4b0e
Mariana Almeida <marianaalmeidafga@gmail.com>
2025-04-20 17:09:31 -0300

    feat: head add

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -17,6 +17,9 @@ import Script from 'next/script';
 import { GA_TRACKING_ID } from '@/src/lib/google-analytics';
 import { Analytics } from '@/src/components/commons/Analytics'; // cria esse componente se for rastrear rotas
 
+// Meta tags - Adiciona aqui as tags de metadados para SEO
+import Head from 'next/head';
+
 const poppins = Poppins({
   subsets: ['latin'],
   weight: ['100', '200', '300', '400', '500', '600', '700', '900'],
@@ -55,6 +58,21 @@ export default async function LocaleLayout({
             `,
           }}
         />
+        {/* Metadados */}
+        <Head>
+          <meta charSet="UTF-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta name="description" content="Portfólio de Mariana Almeida, desenvolvedora de software especializada em front-end e design de interfaces." />
+          <meta name="keywords" content="portfólio, Mariana Almeida, desenvolvedora, front-end, software, design de interfaces, design ux/ui" />
+          <meta name="author" content="Mariana Almeida" />
+          <meta property="og:title" content="Mariana Almeida - Portfólio" />
+          <meta property="og:description" content="Explore o portfólio de Mariana Almeida, com projetos e experiências na área de desenvolvimento de software e design." />
+          <meta property="og:image" content="/favicon.ico" />
+          <meta property="og:url" content="https://www.marialmeida.site/" />
+          <meta name="robots" content="index, follow" />
+          <link rel="icon" href="/favicon.ico" />
+          <title>Portfólio de Mariana Almeida</title>
+        </Head>
       </head>
       <body className={poppins.className}>
         <Navbar />
